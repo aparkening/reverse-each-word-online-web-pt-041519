03d67ebfba856810359d93f906682e7ac0965223
Aaron Parkening <aaron.parkening@gmail.com>
2019-04-18 15:10:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,5 @@
 
 def reverse_each_word(sentence)
   arr = sentence.split
-  new_sentence = ""
-  arr.collect do |word|
-    word.reverse
-  end
-  arr.join()
+  arr.collect { |word| word.reverse }.join(" ")
 end
